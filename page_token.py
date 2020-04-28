5a72c4cec939e4229f45dc91c394a44e3de8939d
lhaowen <39747681+lhaowen@users.noreply.github.com>
2020-04-28 11:22:11 -0700

    Add files via upload

--- page_token.py
+++ page_token.py
@@ -11,18 +11,29 @@ https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-
 def page_token(url):
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html)
+
+    # kill all script and style  elements
     for script in soup(["script", "style"]):
         script.extract()
-    
+
+    # get text
     text = soup.get_text()
-    text = text.strip().lower().split()
+    text_str = ""
+    for line in text.splitlines():
+        for ch in line.lower()
+            if not ch.isascii() or not char.isalnum:
+                text_str += " "
+            else:
+                text_str += ch
+
+    # compute word frequencies
     num = {}
     word_count = 0
     for i in text:
         if i in stopwords:
             text.remove(i)
-            if i not in num:
-                num[i] = text.count(i)
+        if i not in num:
+            num[i] = text.count(i)
         word_count += 1
     
     num = sorted(num.items(), key = lambda x: x[1], reverse = True)
