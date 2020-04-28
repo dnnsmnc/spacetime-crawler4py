c9b3fb71571f2bb0f2abb7b7a032ccf664c4f47f
Adrienne <deniseaz@uci.edu>
2020-04-27 23:35:38 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -168,7 +168,7 @@ def is_valid(url):
         checkext = not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|jpg|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mat|mkv|ogg|ogv|pdf"
+            + r"|wav|avi|mov|mpeg|mpg|ram|m4v|mat|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|ppsx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
