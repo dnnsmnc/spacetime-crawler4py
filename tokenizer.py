a412e406950a959e89cc7e7ad521513d90fb5b3a
Adrienne <deniseaz@uci.edu>
2020-05-22 16:15:44 -0700

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -29,14 +29,19 @@ def page_token(url):
             table = str.maketrans('', '', string.punctuation)
             strippedtext = [w.translate(table) for w in text]
 
+
             for i in strippedtext:
                 if i in stop_words or len(i) < 3:
                     strippedtext.remove(i)
+
+            if len(strippedtext) < 200:
+                return len(strippedtext)
+
+            for i in strippedtext:
+                if i not in scraper.words:
+                    scraper.words[i] = 1
                 else:
-                    if i not in scraper.words:
-                        scraper.words[i] = 1
-                    else:
-                        scraper.words[i] += 1
+                    scraper.words[i] += 1
 
             if scraper.longest_page:
                 if len(strippedtext) > scraper.longest_page[1]:
@@ -44,6 +49,6 @@ def page_token(url):
             else:
                 scraper.longest_page = (url, len(strippedtext))
     except:
-        return
+        return 200
 
-    return
+    return len(strippedtext)
